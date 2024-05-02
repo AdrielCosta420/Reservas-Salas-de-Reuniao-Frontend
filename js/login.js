8b0191ad9cb56c4cd6c663c1c6614b6c1475b7c3
Adriel Silva <adrielsilva_1990@hotmail.com>
2024-05-02 00:51:25 -0300

    create project: add login

---
+++ js/login.js
@@ -0,0 +1,26 @@
+function login(email, password) {
+    fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Falha ao autenticar');
+        }
+        return response.json();
+    }).then(data => {
+        console.log('Token de autentificação: ', data.token);
+        window.location.href = 'meeting_rooms.html';
+    }).catch(error => {
+        console.error('Erro ao autenticar: ', error);
+    })
+}
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+})
